d2ced25c7b0e3fbae2a458e1157d93bb2221f956
yuva25123 <yuvadharshiniakila@gmail.com>
2026-04-14 20:04:48 +0530

    Restore react lab files after renumber

---
+++ AniVerseX/labs/lab-04-react-api-list-saas/frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AnimeList from "./components/AnimeList";
+import AddAnime from "./components/AddAnime";
+
+function App() {
+  return (
+    <div>
+      <AddAnime />
+      <AnimeList />
+    </div>
+  );
+}
+
+export default App;
